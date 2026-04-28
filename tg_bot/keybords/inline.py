267a117009e2b6f364483c894fa1781fcfbcbc31
Ruzik707 <R89061187131@gmail.com>
2026-04-28 23:34:32 +0300

    final commit

--- tg_bot/keybords/inline.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def get_example_keyboard() -> InlineKeyboardMarkup:
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="📋 Пример заполнения", callback_data="example")],
-        [InlineKeyboardButton(text="❓ Помощь", callback_data="help")]
-    ])
-    return keyboard
